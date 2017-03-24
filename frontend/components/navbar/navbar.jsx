bf627547d866bd5b6b520caf93c432b7378e6ad0
Colin Ritchey <colinritchey256@gmail.com>
2017-03-23 17:50:48 -0700

    Fix logout

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -3,22 +3,37 @@ import { Link, hashHistory } from 'react-router';
 
 import SearchFormContainer from '../search_form/search_form_container';
 
-const Navbar = ({currentUser, logout}) => {
-  if(currentUser){
-    return(
-      <div className="navbar">
-        <section>
-          <h1><Link to={'/'}>Quorums</Link></h1>
-          <SearchFormContainer />
-          <input onClick={logout} value="Log Out"></input>
-        </section>
-      </div>
-    );
-  } else {
-    return(
-      <div className="navbar-empty"></div>
-    );
+
+class Navbar extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.handleLogOut = this.handleLogOut.bind(this);
   }
-};
+
+  handleLogOut(){
+    this.props.logout();
+  }
+
+  render(){
+
+    if(this.props.currentUser){
+      return(
+        <div className="navbar">
+          <section>
+            <h1><Link to={'/'}>Quorums</Link></h1>
+            <SearchFormContainer />
+            <input onClick={this.handleLogOut} value="Log Out"></input>
+          </section>
+        </div>
+      );
+    } else {
+      return(
+        <div className="navbar-empty"></div>
+      );
+    }
+
+  }
+}
 
 export default Navbar;
