478eec34e223ef915e862e82e728f08ee9aff1ee
Colin Ritchey <colinritchey256@gmail.com>
2017-05-08 20:22:17 -0700

    added to session, tagging and subscription controller specs

--- spec/controllers/api/sessions_controller_spec.rb
+++ spec/controllers/api/sessions_controller_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Api::SessionsController, type: :controller do
+  let!(:user){ create(:user) }
+  # rendered_response = JSON.parse(response.body)
+  # expect(rendered_response).to eq("Invalid Login")
 
+
+  describe 'POST #create' do
+    context "with invalid credentials" do
+      it "returns to sign in with an non-existent user" do
+        post :create, user: {username: "jill_bruce", password: "abcdef"}
+        rendered_response = JSON.parse(response.body)
+        expect(rendered_response).to eq(["Invalid Login"])
+      end
+
+      it "returns to sign in on bad password" do
+        post :create, user: {username: "breakfast", password: "notmypassword"}
+        rendered_response = JSON.parse(response.body)
+        expect(rendered_response).to eq(["Invalid Login"])
+      end
+    end
+
+    context "with valid credentials" do
+      it "redirects user to links index on success" do
+        post :create,
+          user: {
+            username: "breakfast",
+            password: "password"},
+          format: :json
+          
+        should render_template(:show)
+        should respond_with(200)
+      end
+
+      it "logs in the user" do
+        post :create,
+          user: {
+            username: "breakfast",
+            password: "password"},
+          format: :json
+
+        user = User.find_by_username("breakfast")
+
+        expect(session[:session_token]).to eq(user.session_token)
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+
+  end
 end
--- spec/controllers/api/taggings_controller_spec.rb
+++ spec/controllers/api/taggings_controller_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe Api::TaggingsController, type: :controller do
+  let!(:tag) { create(:tag) }
+  let! (:user) { create(:user)}
+  let! (:question) { create(:question, user_id: user.id) }
 
+  describe 'POST #create' do
+    render_views
+
+
+    before(:each) do
+      post :create,
+        tagging: {
+          tag_id: tag.id,
+          question_id: question.id
+        },
+        format: :json
+    end
+
+    it 'creates the new tagging' do
+      expect(Tagging.exists?).to be true
+      expect(Tagging.first.tag.name).to eq('Testing')
+      expect(Tagging.first.question.title).to eq('Test')
+    end
+  end
+
+  describe 'DELETE #delete' do
+
+    let!(:tagging){
+      create(:tagging,
+        tag_id: tag.id,
+        question_id: question.id)
+    }
+
+    before do
+      delete :destroy, id: tagging.id, format: :json
+    end
+
+    it "removes the comment and redirects back to the link" do
+      expect(Tagging.exists?(tagging.id)).to be false
+    end
+
+  end
 end
